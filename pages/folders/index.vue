79b31934343c0bbb78a87ad19f5a65863dcc86d9
Pieter K de Villiers <pieter@hey.com>
2025-01-28 18:38:05 +0000

    Resize folders and add folder filtering

--- pages/folders/index.vue
+++ pages/folders/index.vue
@@ -8,52 +8,70 @@
       variant="outline">
     </UButton>
   </div>
-  <div class="grid grid-cols-4 gap-5">
-      <div v-for="folder in folders.folders">
-        <FolderCard :folder = "folder"/>
-      </div>
+  <div>
+    <div class="flex px-3 py-3.5">
+        <UInput v-model="q" placeholder="Filter folders..." />
+    </div>
+    <div class="grid grid-cols-6 gap-5">
+    <div v-for="folder in filteredFolders" :key="folder.id">
+        <FolderCard :folder="folder" />
+    </div>
+</div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useAuthStore } from '~/stores/auth';
-const authStore = useAuthStore();
+  import { useAuthStore } from '~/stores/auth';
+  const authStore = useAuthStore();
 
-const account_unique_id = authStore.uniqueAccountId
-const apiAuthorizationToken = authStore.access_token;
-  // Fetch folders data with headers
-const { data: folders, error } = await useFetch(`https://fastapi-rag-2705cfd4c41a.herokuapp.com/api/v1/folders/${account_unique_id}`, {
-method: 'GET',
-headers: {
-  'accept': 'application/json',
-  'Authorization': `Bearer ${apiAuthorizationToken}`,
-}
-});
-
-// Check for errors
-if (error.value) {
-console.error('Error fetching folders:', error.value);
-} else {
-// Check if the response has the expected structure
-if (folders.value) {
-  console.log('Folders:', folders.value);
-  console.log('Stored Unique Account ID:', authStore.uniqueAccountId);
-} else {
-  console.error('Unexpected response format:', folders.value);
-}
-}
-
-</script>
-
-<style scoped>
-  h2 {
-      color: blue;
-      margin-bottom: 20px;
-      font-size: 36px;
+  const account_unique_id = authStore.uniqueAccountId
+  const apiAuthorizationToken = authStore.access_token;
+    // Fetch folders data with headers
+  const { data: folders, error } = await useFetch(`https://fastapi-rag-2705cfd4c41a.herokuapp.com/api/v1/folders/${account_unique_id}`, {
+  method: 'GET',
+  headers: {
+    'accept': 'application/json',
+    'Authorization': `Bearer ${apiAuthorizationToken}`,
   }
-  p {
-      color: green;
-      margin: 20px 0;
+  });
+
+  // Check for errors
+  if (error.value) {
+  console.error('Error fetching folders:', error.value);
+  } else {
+  // Check if the response has the expected structure
+  if (folders.value) {
+    console.log('Folders:', folders.value);
+    console.log('Stored Unique Account ID:', authStore.uniqueAccountId);
+  } else {
+    console.error('Unexpected response format:', folders.value);
   }
+  }
+
+  const q = ref('');
+
+  const filteredFolders = computed(() => {
+      if (!q.value) {
+          return folders.value.folders
+      }
+      return folders.value.folders.filter((folder) => {
+          return Object.values(folder).some((value) => {
+          return String(value).toLowerCase().includes(q.value.toLowerCase())
+          })
+      })
+      })
+
+  </script>
+
+  <style scoped>
+    h2 {
+        color: blue;
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        color: green;
+        margin: 20px 0;
+    }
 
 </style>
